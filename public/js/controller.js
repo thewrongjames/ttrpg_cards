d78dd1695a8efe9e934435f8d392b495310b0740
James Wright <hello@jameswright.me>
2024-01-24 08:25:57 +1100

    Add initial web-component setup
    Very basic, but I do seem to be able to get a name and type showing up
    through a web-component, using the listener setup.

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,1 +1,18 @@
-console.log('hi')
+import { CardComponent } from '/js/views/card-component.js'
+import { Card } from '/js/models/card.js'
+
+function setup() {
+  const container = document.getElementById('web-component-test')
+  if (container === null) {
+    throw new Error('no element with the ID "web-component-test"')
+  }
+
+  const card = new Card()
+  const cardComponent = new CardComponent(card)
+  container.appendChild(cardComponent)
+
+  card.name = 'something'
+  card.type = 'something else'
+}
+
+setup()
---
+++ public/js/views/card-component.js
@@ -0,0 +1,34 @@
+/** @typedef {import("/js/models/card.js").Card} Card */
+
+export class CardComponent extends HTMLElement {
+  #card
+  
+  /** @param {Card} card */
+  constructor(card) {
+    super()
+
+    this.#card = card
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({mode: 'open'})
+
+    const container = document.createElement('div')
+    container.setAttribute('class', 'card')
+
+    const name = document.createElement('p')
+    name.setAttribute('class', 'card-name')
+
+    const type = document.createElement('p')
+    type.setAttribute('class', 'card-type')
+
+    this.#card.subscribe('name', () => name.innerText = this.#card.name)
+    this.#card.subscribe('type', () => type.innerText = this.#card.type)
+
+    shadow.appendChild(container)
+    container.appendChild(name)
+    container.appendChild(type)
+  }
+}
+
+customElements.define('card-component', CardComponent)
